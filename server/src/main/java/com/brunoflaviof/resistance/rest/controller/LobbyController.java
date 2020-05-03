134de04acb17d58aee870cecebfd29497fcdd074
Bruno <exilio@brunoflaviof.com>
2020-05-03 13:15:44 -0300

    [Fix]: Authentication now requires userId

--- server/src/main/java/com/brunoflaviof/resistance/rest/controller/LobbyController.java
+++ server/src/main/java/com/brunoflaviof/resistance/rest/controller/LobbyController.java
@@ -5,14 +5,15 @@ import com.brunoflaviof.resistance.rest.model.LobbyList;
 import com.brunoflaviof.resistance.rest.repository.data.Lobby;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.UUID;
+
 public interface LobbyController {
 
     @GetMapping("/lobbies")
     public LobbyList getLobbies();
 
     @PutMapping("/lobby")
-    public void createLobby(@RequestBody CreateLobby lobby);
+    public void createLobby(@RequestHeader("userId") String userId, @RequestBody CreateLobby lobby);
 
     @GetMapping("/lobby")
     public Lobby getLobbyByName(@RequestParam String name);
--- server/src/main/java/com/brunoflaviof/resistance/rest/jwt/JWTUtil.java
+++ server/src/main/java/com/brunoflaviof/resistance/rest/jwt/JWTUtil.java
@@ -4,7 +4,9 @@ import com.brunoflaviof.resistance.rest.repository.data.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -12,42 +14,44 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Service
 public class JWTUtil implements Serializable {
     private static final long JWT_TOKEN_VALIDITY = 1000 * 60 * 60 * 5;
     @Value("${security.jwt.secret}")
-    private static String secret;
+    private String secret;
 
-    public static String generateToken(User user){
+    public  String generateToken(User user){
         Map<String, Object>tokenData = new HashMap<>();
         tokenData.put("userID", user.getUserID());
         tokenData.put("name", user.getName());
-        return Jwts.builder().setClaims(tokenData)
+        String token = Jwts.builder().setClaims(tokenData)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
+        return token;
     }
 
-    public static UUID getUserIDFromToken(String token){
+    public  UUID getUserIDFromToken(String token){
         Claims tokenData = getClaims(token);
         return UUID.fromString((String) tokenData.get("userID"));
     }
 
-    public static boolean validateToken(String token, User user){
+    public  boolean validateToken(String token, User user){
         Claims tokenData = getClaims(token);
         if(isExpired(tokenData))
             return false;
         return user.getUserID().equals(tokenData.get("userID"));
     }
 
-    private static Claims getClaims(String token) {
+    private  Claims getClaims(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
-    private static boolean isExpired(Claims tokenData) {
+    private  boolean isExpired(Claims tokenData) {
         return tokenData.getExpiration().before(new Date());
     }
 
-    public static void setSecret(String secret) {
-        JWTUtil.secret = secret;
+    public void setSecret(String secret) {
+        this.secret = secret;
     }
 }
